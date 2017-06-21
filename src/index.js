68a55e598fc9fd3b3866fd654ffbc7e8ef570956
Pablo Jurado <pjurado@overnitecbt.com>
2017-06-21 14:13:53 -0500

    change from class components to functional

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import './index.css'
+import firebase from './firebase.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+let appState = {
+  currentItem: '',
+  username: '',
+  items: []
+}
+
+function updateDB() {
+  const itemsRef = firebase.database().ref('items');
+  itemsRef.on('value', (snapshot) => {
+    let items = snapshot.val();
+    let newState = [];
+    for (let item in items) {
+      newState.push({
+        id: item,
+        title: items[item].title,
+        user: items[item].user
+      });
+    }
+    appState.items = newState
+  })
+}
+
+// ---------------------------------------------------------
+// Render Loop
+// ---------------------------------------------------------
+
+const rootEl = document.getElementById('root')
+
+function renderNow () {
+  updateDB()
+  ReactDOM.render(App(appState), rootEl)
+  window.requestAnimationFrame(renderNow)
+}
+
+window.requestAnimationFrame(renderNow)
+
+export default appState
