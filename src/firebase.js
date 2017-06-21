3d0e3347b3a908605ed71e40d51cd86fa7f790dd
Pablo Jurado <pjurado@overnitecbt.com>
2017-06-21 13:23:15 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBhKmNthC0D54AWeNN_ZqnNwx4oQCqx6FI",
+    authDomain: "food-log-fe124.firebaseapp.com",
+    databaseURL: "https://food-log-fe124.firebaseio.com",
+    projectId: "food-log-fe124",
+    storageBucket: "food-log-fe124.appspot.com",
+    messagingSenderId: "173394592537"
+  };
+  firebase.initializeApp(config);
+
+export default firebase
